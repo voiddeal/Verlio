c7fe117ec8e06fa3b1688d8e9defcdad31c052a8
Void Dealer <hr.shafiei.dev@gmail.com>
2025-03-13 16:47:12 +0330

    Update next.config.ts
    Allow fetching meal images via fetch API by updating remotePatterns

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
   /* config options here */
-};
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "www.themealdb.com",
+        port: "",
+        pathname: "**",
+        search: "",
+      },
+    ],
+  },
+}
 
-export default nextConfig;
+export default nextConfig
