442bc3c47a6c66d1f6ce773918583be77aa67796
Void Dealer <hr.shafiei.dev@gmail.com>
2025-03-13 16:06:50 +0330

    Update tailwin.config - Add colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,10 +7,35 @@ export default {
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      textColor: {
+        primary: {
+          light: "#818181",
+          default: "#272727",
+        },
       },
+      colors: {
+        theme: {
+          green: {
+            light: "#a5ffa5",
+            default: "#6fff6f",
+            dark: "#009900",
+          },
+          blue: {
+            light: "#94e0ff",
+            default: "#60d1ff",
+          },
+        },
+      },
+      fontSize: {
+        xs: "0.7rem",
+      },
+    },
+    screens: {
+      sm: "576px", // Replace 'sm' breakpoint with your custom value
+      md: "768px", // Replace 'md' breakpoint
+      lg: "992px", // Replace 'lg' breakpoint
+      xl: "1200px", // Replace 'xl' breakpoint
+      "2xl": "1366px", // Replace '2xl' breakpoint
     },
   },
   plugins: [],
