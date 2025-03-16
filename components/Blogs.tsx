21e89fe24117ea15d63e7eeb6f6b2540c0c390a7
Void Dealer <hr.shafiei.dev@gmail.com>
2025-03-16 23:51:44 +0330

    Update Blogs.tsx

--- components/Blogs.tsx
+++ components/Blogs.tsx
@@ -1,27 +1,34 @@
 import Image from "next/image"
-import stew from "@/public/blog-beef-dumpling-stew/1.jpg"
-import fajita from "@/public/blog-chicken-fajita/1.jpg"
-import ratatouille from "@/public/blog-ratatouille/1.jpg"
-import shakshuka from "@/public/blog-shakshuka/1.jpg"
+import stew from "@/public/blogs/beef-dumpling-stew/1.jpg"
+import fajita from "@/public/blogs/chicken-fajita/1.jpg"
+import ratatouille from "@/public/blogs/ratatouille/1.jpg"
+import shakshuka from "@/public/blogs/shakshuka/1.jpg"
 import Link from "next/link"
 
 export default function Blogs() {
   return (
-    <div className="py-5 flex flex-col justify-center itm">
+    <section className="py-5 flex flex-col justify-center itm">
+      {/* Blog Header */}
       <div className="flex justify-between px-5">
         <h4>Recipe Blogs</h4>
         <button type="button">View More</button>
       </div>
-      <div className="flex justify-center gap-x-6 p-5">
-        <Link href="#" className="flex flex-col justify-center items-center">
+      {/* Content */}
+      <div className="flex flex-col md:flex-row justify-center gap-x-6 p-5">
+        {/* Main post */}
+        <Link
+          href="#"
+          className="w-full md:w-[60%] flex flex-col justify-center items-center m-auto md:m-[unset]"
+        >
           <Image
             src={stew}
-            width={400}
-            height={300}
+            layout="responsive"
             alt="blog preview image"
-            className="rounded-xl"
+            width={400}
+            height={400}
+            className="rounded-xl w-full"
           />
-          <div className="flex flex-col self-start py-4 pr-2">
+          <div className="flex flex-col self-start pt-2 pb-8 p md:py-4 md:pr-2">
             <span className="text-xs text-primary-light pb-1">
               November 16, 2022
             </span>
@@ -31,8 +38,12 @@ export default function Blogs() {
             </span>
           </div>
         </Link>
-        <div className="w-[40%] flex flex-col justify-between items-center">
-          <Link href="#" className="w-full flex justify-center gap-x-2">
+        {/* Posts */}
+        <div className="w-full md:w-[40%] flex md:flex-col justify-center md:justify-between gap-x-2">
+          <Link
+            href="#"
+            className="flex-1 md:flex-initial flex flex-col md:flex-row justify-center items-center gap-y-1 md:gap-y-0 md:gap-x-2 bg-theme-blue-light rounded-md p-4"
+          >
             <Image
               src={shakshuka}
               width={120}
@@ -40,56 +51,62 @@ export default function Blogs() {
               alt="blog preview image"
               className="rounded-xl"
             />
-            <div className="w-[60%] flex flex-col">
-              <span className="text-xs text-primary-light flex-1">
+            <div className="w-full md:w-[60%] flex-1 flex flex-col">
+              <span className="text-xs text-primary-light">
                 November 16, 2022
               </span>
-              <span className="">title</span>
+              <span className="mb-auto">title</span>
               <span className="text-xs text-primary-light line-clamp-2">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores
+                1 ipsum dolor sit amet consectetur adipisicing elit. Maiores
                 totam nulla quibusdam.
               </span>
             </div>
           </Link>
-          <Link href="#" className="w-full flex justify-center gap-x-2">
+          <Link
+            href="#"
+            className="flex-1 md:flex-initial flex flex-col md:flex-row justify-center items-center gap-y-1 md:gap-y-0 md:gap-x-2 bg-theme-blue-light rounded-md p-4"
+          >
             <Image
               src={ratatouille}
               width={120}
               height={120}
               alt="blog preview image"
-              className="rounded-xl"
+              className="rounded-xl flex-1"
             />
-            <div className="w-[60%] flex flex-col">
-              <span className="text-xs text-primary-light flex-1">
+            <div className="w-full md:w-[60%] flex-1 flex flex-col">
+              <span className="text-xs text-primary-light">
                 November 16, 2022
               </span>
-              <span className="">title</span>
+              <span className="mb-auto">title</span>
               <span className="text-xs text-primary-light line-clamp-2">
                 Lorem ipsum dolor amet elit.
               </span>
             </div>
           </Link>
-          <Link href="#" className="w-full flex justify-center gap-x-2">
+          <Link
+            href="#"
+            className="flex-1 md:flex-initial sm:flex flex-col md:flex-row justify-center items-center gap-y-1 md:gap-y-0 md:gap-x-2 bg-theme-blue-light rounded-md p-4 hidden"
+          >
             <Image
               src={fajita}
               width={120}
               height={120}
               alt="blog preview image"
-              className="rounded-xl"
+              className="rounded-xl flex-1"
             />
-            <div className="w-[60%] flex flex-col">
-              <span className="text-xs text-primary-light flex-1">
+            <div className="w-full md:w-[60%] flex-1 flex flex-col">
+              <span className="text-xs text-primary-light">
                 November 16, 2022
               </span>
-              <span className="">title</span>
+              <span className="mb-auto">title</span>
               <span className="text-xs text-primary-light line-clamp-2">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores
-                totam nulla quibusdam.
+                Lorem ipsum dolor sit adipisicing elit. Maiores totam nulla
+                quibusdam.
               </span>
             </div>
           </Link>
         </div>
       </div>
-    </div>
+    </section>
   )
 }
