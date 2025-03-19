ac520a34814e62f580edcd8e6ea464b7a7d7a948
Void Dealer <hr.shafiei.dev@gmail.com>
2025-03-20 00:06:08 +0330

    Cleanup deleted files

--- utils/formattedDate.ts
+++
@@ -1,8 +0,0 @@
-export default function formattedDate(date: string) {
-  return new Date(date).toLocaleDateString("en-CA", {
-    timeZone: "America/Vancouver",
-    weekday: "long",
-    day: "2-digit",
-    month: "long",
-  })
-}
