e03ad73e14fe182a4c482a5521b8233d5d076787
Void Dealer <hr.shafiei.dev@gmail.com>
2025-03-25 22:06:29 +0330

    Update root page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,24 @@
-import FeaturedMeals from "@/components/FeaturedMeals"
 import Hero from "@/components/hero/Hero"
+import Records from "@/components/Records"
+import FeaturedMeals from "@/components/FeaturedMeals"
 import EatOurFood from "@/components/eat-our-food/EatOurFood"
 import Gallery from "@/components/Gallery"
 import Blogs from "@/components/Blogs"
+import HowItWorks from "./reservation/HowItWorks"
+import Experience from "@/components/experience/Experience"
 
 export default function Home() {
   return (
     <main className="relative overflow-x-hidden">
+      <div className="absolute w-full h-full bg-[url('/food-pattern-light.jpg')] bg-repeat bg-contain bg-center opacity-10 -z-10"></div>
       <Hero />
-      <EatOurFood />
+      <Records />
+      <Experience />
       <FeaturedMeals />
+      <EatOurFood />
       <Blogs />
       <Gallery />
+      <HowItWorks />
     </main>
   )
 }
