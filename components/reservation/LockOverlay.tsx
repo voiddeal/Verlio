85a915b3f15b1ce75330035a1d4efb314b4f7127
Void Dealer <hr.shafiei.dev@gmail.com>
2025-03-25 21:59:28 +0330

    Index change for LockOverlay.tsx

---
+++ components/reservation/LockOverlay.tsx
@@ -0,0 +1,28 @@
+"use client"
+
+import { useAppSelector } from "@/redux/hooks"
+import { CiLock } from "react-icons/ci"
+
+interface Props {
+  originStep: number
+}
+
+export default function LockOverlay({ originStep }: Props) {
+  const { currentReservationStep } = useAppSelector((state) => state.app)
+  const overlay = (
+    <div
+      className={`absolute w-full h-full bg-black backdrop-blur-sm text-center text-white text-lg z-10 ${
+        originStep === 2 ? "bg-opacity-80" : "bg-opacity-60"
+      }`}
+    >
+      <CiLock className="inline-block m-auto size-28 mt-10" />
+    </div>
+  )
+  const one = currentReservationStep === 1 && originStep === 2
+  const two = currentReservationStep === 2 && originStep === 1
+
+  if (one) return overlay
+  if (two) return overlay
+
+  return null
+}
