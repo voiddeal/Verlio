974e23ed1f23f37bde83ccc560b84a2e9528c921
Void Dealer <hr.shafiei.dev@gmail.com>
2025-03-25 21:46:26 +0330

    also return hours and minutes from dataFormatter

--- utils/dateFormatter.ts
+++ utils/dateFormatter.ts
@@ -1,6 +1,8 @@
 interface DateFormatterResult {
   iso: string
   display: string
+  hours: number
+  minutes: number
 }
 
 const timeZone = "America/Vancouver"
@@ -11,6 +13,7 @@ const isoDate = new Intl.DateTimeFormat("en-CA", {
   month: "2-digit",
   day: "2-digit",
 })
+
 const displayDate = new Intl.DateTimeFormat("en-CA", {
   timeZone,
   weekday: "long",
@@ -18,14 +21,26 @@ const displayDate = new Intl.DateTimeFormat("en-CA", {
   month: "long",
 })
 
+const hour = new Intl.DateTimeFormat("en-US", {
+  hour: "2-digit",
+})
+
+const minute = new Intl.DateTimeFormat("en-US", {
+  minute: "2-digit",
+})
+
 export default function dateFormatter(
   date: Date = new Date()
 ): DateFormatterResult {
   const iso = isoDate.format(date)
   const display = displayDate.format(date)
+  const hours = hour.format(date)
+  const minutes = minute.format(date)
 
   return {
     iso,
     display,
+    hours: parseInt(hours),
+    minutes: parseInt(minutes),
   }
 }
