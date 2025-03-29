fe83a68e2860fb50dd2b7a7ade18e3c21a186b4e
Void Dealer <hr.shafiei.dev@gmail.com>
2025-03-29 21:22:09 +0330

    Add root loading.tsx

---
+++ app/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <p className="text-xl font-medium">Loading...</p>
+    </div>
+  )
+}
