94fed44af926eed0f5443fe27ea50507f92eba54
Void Dealer <hr.shafiei.dev@gmail.com>
2025-03-31 01:19:32 +0330

    Increase top nav bg trigger threshold

--- components/header/TopNavBG.tsx
+++ components/header/TopNavBG.tsx
@@ -7,7 +7,7 @@ export default function TopNavBG() {
   return (
     <div
       className={`absolute w-full top-0 bg-[#e4b257] transition-transform -z-10 duration-300 ${
-        y > 100 ? "translate-y-0" : "-translate-y-[100%]"
+        y > 400 ? "translate-y-0" : "-translate-y-[100%]"
       }`}
     >
       <div className="px-4 py-3 cursor-pointer w-fit ml-auto">
