80b8b7face82f0ea24ee78912b417d89a978f46d
Void Dealer <hr.shafiei.dev@gmail.com>
2025-03-31 01:18:59 +0330

    Parallax hero

--- components/hero/Hero.tsx
+++ components/hero/Hero.tsx
@@ -1,33 +1,40 @@
-import HeroInfoCards from "../hero-info-cards/HeroInfoCards"
+"use client"
+
+import useScrollTracker from "@/utils/useScrollTracker"
+import InfoCards from "../info-cards/InfoCards"
 import Image from "next/image"
 
 export default function Hero() {
+  const y = useScrollTracker()
   return (
-    <div className="relative bg-black h-full">
+    <div className="relative z-30 w-dvw h-dvh overflow-hidden">
       {/* Hero Image */}
-      <div className="relative w-dvw h-dvh">
+      <div
+        className="absolute w-full h-full will-change-transform"
+        style={{
+          transform: `translateY(${y * 0.5}px)`,
+        }}
+      >
         <Image
           id="hero-img"
-          // src="/hero/hero-image-default.jpg"
           src="/hero/hero-image.jpg"
-          width={1920}
-          height={1080}
+          fill
           quality={100}
           alt="hero-image"
-          className="grayscale-[30%] w-full h-full object-cover "
+          className="grayscale-[30%]  object-cover"
           priority
         />
-        <div className="absolute inset-0 bg-gradient-to-b from-black via-transparent to-transparent opacity-80"></div>
       </div>
+      <div className="absolute inset-0 bg-gradient-to-b from-black/80 via-transparent to-transparent"></div>
       <div className="absolute w-full h-full top-0 md:flex justify-between">
-        <h1 className="w-fit m-auto mt-[13dvh] text-white">
+        <h1 className="w-fit m-auto mt-[13dvh] text-white lg:scale-[1.5] 2xl:scale-[2] ">
           <span className="text-[10rem] leading-[11rem] monsieur-la-doulaise-regular">
             Verlio
           </span>
           <br />
           <span className="text-2xl leading-none capitalize">RESTAURANT</span>
         </h1>
-        <HeroInfoCards />
+        {/* <InfoCards /> */}
       </div>
       {/* <Image
         src={"/verlio.png"}
