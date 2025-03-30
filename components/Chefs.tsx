9ac8994bc3f088660952d1b7a355293b4466aa0a
Void Dealer <hr.shafiei.dev@gmail.com>
2025-03-30 23:39:24 +0330

    Update Chefs.tsx

--- components/Chefs.tsx
+++ components/Chefs.tsx
@@ -3,107 +3,71 @@ import { FaXTwitter } from "react-icons/fa6"
 import { FaFacebook } from "react-icons/fa6"
 import { FaInstagram } from "react-icons/fa6"
 import { FaLinkedinIn } from "react-icons/fa6"
+import CoverLayer from "./cover-layer-animation/CoverLayerAnimation"
+import chefs from "@/db/chefs.json"
 
 export default function Chefs() {
+  const socialLinkClasses =
+    "p-1 lg:p-2 rounded-full border-2 lg:border-8 border-transparent backdrop-blur-[2px] outline outline-2 outline-white cursor-pointer text-white"
+
+  const socialIconClasses = "size-4 sm:size-6 bg-transparent transition-none"
+
+  const chefJSX = chefs.map((chef) => {
+    const { name, role, img, socials } = chef
+    return (
+      <div className="group h-full">
+        <div className="relative overflow-hidden rounded-3xl size-full">
+          <Image
+            src={img}
+            alt="Freja Charles"
+            width={500}
+            height={500}
+            className="size-full object-cover"
+          />
+          <div className="absolute size-full inset-0 m-auto opacity-0 group-hover:opacity-100 flex flex-col justify-center items-center z-20 bg-black/0  group-hover:bg-black/60 gap-y-2">
+            <a
+              href={socials.twitter || undefined}
+              className={`${socialLinkClasses} hover:outline-cyan-400`}
+            >
+              <FaXTwitter className={socialIconClasses} />
+            </a>
+            <a
+              href={socials.facebook || undefined}
+              className={`${socialLinkClasses} hover:outline-blue-600`}
+            >
+              <FaFacebook className={socialIconClasses} />
+            </a>
+            <a
+              href={socials.instagram || undefined}
+              className={`${socialLinkClasses} hover:outline-red-500`}
+            >
+              <FaInstagram className={socialIconClasses} />
+            </a>
+            <a
+              href={socials.linkedin || undefined}
+              className={`${socialLinkClasses} hover:outline-blue-400`}
+            >
+              <FaLinkedinIn className={socialIconClasses} />
+            </a>
+          </div>
+        </div>
+        <div className="w-full h-full">
+          <span className="block text-center py-2">{name}</span>
+          <small className="block text-center text-orange">{role}</small>
+        </div>
+      </div>
+    )
+  })
+
   return (
-    <section className="py-10 w-11/12 sm:w-5/6 md:w-2/3 mx-auto">
-      <div className="w-10/12 mx-auto bg-[url('/linebreak-2.png')] bg-contain bg-center bg-no-repeat h-6"></div>
-      <h4 className="text-3xl text-center pb-8">
-        <span className="text-orange">The Masters</span> Behind the Apron
-      </h4>
-      <div className="flex justify-center gap-x-4 px-2">
-        <div className="group transition-all">
-          <div className="relative overflow-hidden rounded-3xl w-full h-full">
-            <Image
-              src={"/chefs/freja-charles.jpg"}
-              alt="Freja Charles"
-              width={500}
-              height={500}
-              className="w-full h-full aspect-[3/4] object-cover"
-            />
-            <div className="absolute inset-x-0 mx-auto bottom-0 translate-y-[calc(100%+10px)] group-hover:-translate-y-4 duration-300 flex gap-x-3 justify-center place-items-center z-20">
-              <div className="p-2 rounded-full border-8 border-transparent backdrop-blur-[2px] outline outline-2 outline-white hover:outline-cyan-400 cursor-pointer text-white">
-                <FaXTwitter className=" size-2 bg-transparent" />
-              </div>
-              <div className="p-2 rounded-full border-8 border-transparent backdrop-blur-[2px] outline outline-2 outline-white hover:outline-blue-600 cursor-pointer text-white">
-                <FaFacebook className=" size-2 bg-transparent" />
-              </div>
-              <div className="p-2 rounded-full border-8 border-transparent backdrop-blur-[2px] outline outline-2 outline-white hover:outline-red-500 cursor-pointer text-white">
-                <FaInstagram className=" size-2 bg-transparent" />
-              </div>
-              <div className="p-2 rounded-full border-8 border-transparent backdrop-blur-[2px] outline outline-2 outline-white hover:outline-blue-400 cursor-pointer text-white">
-                <FaLinkedinIn className=" size-2 bg-transparent" />
-              </div>
-            </div>
-            <div className="absolute inset-0 m-auto bg-black/0 transition-colors duration-200 group-hover:bg-black/60 z-10"></div>
-          </div>
-          <div className="">
-            <span className="block text-center py-2">Freja Charles</span>
-            <small className="block text-center text-orange">Pastry Chef</small>
-          </div>
-        </div>
-        <div className="group transition-all">
-          <div className="relative overflow-hidden rounded-b-3xl rounded-t-[50%] w-full h-full">
-            <Image
-              src={"/chefs/hiroshi-tanaka.jpg"}
-              alt="hiroshi-tanaka"
-              width={500}
-              height={500}
-              className="w-full h-full aspect-[3/4] object-cover"
-            />
-            <div className="absolute inset-x-0 mx-auto bottom-0 translate-y-[calc(100%+10px)] group-hover:-translate-y-4 duration-300 flex gap-x-3 justify-center place-items-center z-20">
-              <div className="p-2 rounded-full border-8 border-transparent backdrop-blur-[2px] outline outline-2 outline-white hover:outline-cyan-400 cursor-pointer text-white">
-                <FaXTwitter className=" size-6 bg-transparent" />
-              </div>
-              <div className="p-2 rounded-full border-8 border-transparent backdrop-blur-[2px] outline outline-2 outline-white hover:outline-blue-600 cursor-pointer text-white">
-                <FaFacebook className=" size-6 bg-transparent" />
-              </div>
-              <div className="p-2 rounded-full border-8 border-transparent backdrop-blur-[2px] outline outline-2 outline-white hover:outline-red-500 cursor-pointer text-white">
-                <FaInstagram className=" size-6 bg-transparent" />
-              </div>
-              <div className="p-2 rounded-full border-8 border-transparent backdrop-blur-[2px] outline outline-2 outline-white hover:outline-blue-400 cursor-pointer text-white">
-                <FaLinkedinIn className=" size-6 bg-transparent" />
-              </div>
-            </div>
-            <div className="absolute inset-0 m-auto bg-black/0 transition-colors duration-200 group-hover:bg-black/60 z-10"></div>
-          </div>
-          <div className="">
-            <span className="block text-center py-2">Hiroshi Tanaka</span>
-            <small className="block text-center text-orange">
-              Executive Chef
-            </small>
-          </div>
-        </div>
-        <div className="group transition-all">
-          <div className="relative overflow-hidden rounded-3xl w-full h-full">
-            <Image
-              src={"/chefs/carson-witherspoon.jpg"}
-              alt="Carson Witherspoon"
-              width={500}
-              height={500}
-              className="w-full h-full aspect-[3/4] object-cover"
-            />
-            <div className="absolute inset-x-0 mx-auto bottom-0 translate-y-[calc(100%+10px)] group-hover:-translate-y-4 duration-300 flex gap-x-3 justify-center place-items-center z-20">
-              <div className="p-2 rounded-full border-8 border-transparent backdrop-blur-[2px] outline outline-2 outline-white hover:outline-cyan-400 cursor-pointer text-white">
-                <FaXTwitter className=" size-6 bg-transparent" />
-              </div>
-              <div className="p-2 rounded-full border-8 border-transparent backdrop-blur-[2px] outline outline-2 outline-white hover:outline-blue-600 cursor-pointer text-white">
-                <FaFacebook className=" size-6 bg-transparent" />
-              </div>
-              <div className="p-2 rounded-full border-8 border-transparent backdrop-blur-[2px] outline outline-2 outline-white hover:outline-red-500 cursor-pointer text-white">
-                <FaInstagram className=" size-6 bg-transparent" />
-              </div>
-              <div className="p-2 rounded-full border-8 border-transparent backdrop-blur-[2px] outline outline-2 outline-white hover:outline-blue-400 cursor-pointer text-white">
-                <FaLinkedinIn className=" size-6 bg-transparent" />
-              </div>
-            </div>
-            <div className="absolute inset-0 m-auto bg-black/0 transition-colors duration-200 group-hover:bg-black/60 z-10"></div>
-          </div>
-          <div className="">
-            <span className="block text-center py-2">Carson Witherspoon</span>
-            <small className="block text-center text-orange">Sous Chef</small>
-          </div>
-        </div>
+    <section className="relative my-10">
+      <CoverLayer to="bottom" />
+      <div className="py-10 w-11/12 sm:w-5/6 md:w-2/3 mx-auto">
+        <div className="w-10/12 mx-auto bg-[url('/linebreak-2.png')] bg-contain bg-center bg-no-repeat h-6"></div>
+        <h4 className="text-3xl text-center pb-8">
+          <span className="text-orange">The Masters</span> Behind the Apron
+        </h4>
+        <div className="flex justify-center gap-x-4 px-2">{chefJSX}</div>
       </div>
     </section>
   )
