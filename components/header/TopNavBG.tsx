cfd07442c4fde3f20869d6b2e92813861364a557
Void Dealer <hr.shafiei.dev@gmail.com>
2025-03-31 23:59:27 +0330

    Correct width values for top nav bg

--- components/header/TopNavBG.tsx
+++ components/header/TopNavBG.tsx
@@ -6,14 +6,14 @@ export default function TopNavBG() {
 
   return (
     <div
-      className={`absolute w-full top-0 bg-[#e4b257] transition-transform -z-10 duration-300 ${
+      className={`fixed w-dvw top-0 bg-[#e4b257] transition-transform -z-10 duration-300 ${
         y > 400 ? "translate-y-0" : "-translate-y-[100%]"
       }`}
     >
-      <div className="px-4 py-3 cursor-pointer w-fit ml-auto">
+      <div className="py-3 cursor-pointer w-fit ml-auto">
         <Link
           href="/"
-          className="text-stone-100 hover:text-theme-default block text-2xl"
+          className="pr-8 text-stone-100 hover:text-theme-default text-2xl"
         >
           Verlio
         </Link>
