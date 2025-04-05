4b620d007ac07cb1814e0e70f0fa8017da14ed22
Void Dealer <hr.shafiei.dev@gmail.com>
2025-04-05 17:58:41 +0330

    Update blogs

--- components/Blogs.tsx
+++ components/Blogs.tsx
@@ -7,11 +7,12 @@ export default function Blogs() {
     if (index === 0) return
     const isLast = index + 1 === blogs.length
     const { date, description, href, img, title } = blog
+
     return (
       <Link
         key={date}
         href={`/blogs/${href}`}
-        className={`flex-1 lg:flex-initial flex flex-col lg:flex-row lg:justify-center items-center gap-y-1 lg:gap-y-0 lg:gap-x-2 rounded-lg p-4 ${
+        className={`flex-1 lg:flex-initial flex flex-col lg:flex-row lg:justify-center items-center gap-y-1 lg:gap-y-0 lg:gap-x-2 rounded-lg p-4 hover:scale-105 transition-transform ${
           isLast ? "sm:flex hidden" : ""
         }`}
       >
@@ -32,19 +33,20 @@ export default function Blogs() {
       </Link>
     )
   })
+
   return (
     <section className="py-5 flex flex-col justify-center w-11/12 sm:w-5/6 lg:w-2/3 mx-auto">
       {/* Blog Header */}
-      <div className="flex justify-between px-5">
-        <h4>Recipe Blogs</h4>
-        <button type="button">View More</button>
-      </div>
+      <div className="w-10/12 mx-auto bg-[url('/linebreak-2.png')] bg-contain bg-center bg-no-repeat h-6"></div>
+      <header className="my-8">
+        <h4 className="text-3xl text-center text-orange">Recipe Blogs</h4>
+      </header>
       {/* Content */}
       <div className="flex flex-col lg:flex-row justify-center gap-x-6 p-5">
         {/* Main post */}
         <Link
           href={`/blogs/${blogs[0].href}`}
-          className="w-full lg:w-[60%] flex flex-col justify-center items-center m-auto lg:m-[unset]"
+          className="w-full lg:w-[60%] flex flex-col justify-center items-center m-auto lg:m-[unset] group overflow-hidden rounded-xl transition-transform"
         >
           <Image
             src={blogs[0].img}
@@ -52,7 +54,7 @@ export default function Blogs() {
             alt="blog preview image"
             width={1000}
             height={1000}
-            className="rounded-xl w-full"
+            className="w-full group-hover:scale-105 rounded-[inherit] transition-transform"
           />
           <div className="flex flex-col self-start pt-2 pb-8 p lg:py-4 lg:pr-2">
             <span className="text-xs text-primary-light pb-1">
