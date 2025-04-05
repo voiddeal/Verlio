226deb8d9d47377c54b291222ec65713148de6c6
Void Dealer <hr.shafiei.dev@gmail.com>
2025-04-05 18:25:57 +0330

    Add back to menu button

--- app/menu/[category]/page.tsx
+++ app/menu/[category]/page.tsx
@@ -2,8 +2,10 @@ import FlipCard from "@/components/flip-card/FlipCard"
 import Heading from "@/components/Heading"
 import BackView from "@/components/meal-card/BackView"
 import FrontView from "@/components/meal-card/FrontView"
-import { MealList } from "@/types/api"
+import type { MealList } from "@/types/api"
+import { CgArrowLongRight } from "react-icons/cg"
 import Image from "next/image"
+import Link from "next/link"
 
 interface Props {
   params: Promise<{ category: string }>
@@ -46,6 +48,21 @@ export default async function Category({ params }: Props) {
     )
   })
 
+  const backButton = (
+    <Link
+      href={"/menu"}
+      className="flex items-center py-2 px-4 bg-theme-blue-dark text-white rounded-lg w-fit mx-auto my-5 group hover:bg-theme-blue-dark/90"
+    >
+      <CgArrowLongRight
+        size={20}
+        className="scale-x-[-1] group-hover:-translate-x-2 transition-transform"
+      />
+      <button type="button" className="py-2 px-4">
+        Back to Menu
+      </button>
+    </Link>
+  )
+
   return (
     <main>
       <header className="relative overflow-hidden">
@@ -57,11 +74,13 @@ export default async function Category({ params }: Props) {
           className="object-cover -z-20 object-center"
         />
       </header>
+      {backButton}
       <section className="w-11/12 sm:w-9/12 mx-auto rounded-lg">
         <div className="border-y-2 border-gray-300 my-6 py-4 grid grid-cols-[repeat(auto-fit,_minmax(300px,_1fr))] gap-10">
           {mealElements}
         </div>
       </section>
+      {backButton}
     </main>
   )
 }
