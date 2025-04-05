b13132acb8914aa2d557ed7f5259e60f4250aa2e
Void Dealer <hr.shafiei.dev@gmail.com>
2025-04-05 17:52:54 +0330

    Reduce TNBG visibility threshold

--- components/header/TopNavBG.tsx
+++ components/header/TopNavBG.tsx
@@ -7,7 +7,7 @@ export default function TopNavBG() {
   return (
     <div
       className={`fixed w-dvw top-0 bg-[#e4b257] transition-transform -z-10 duration-300 ${
-        y > 400 ? "translate-y-0" : "-translate-y-[100%]"
+        y > 200 ? "translate-y-0" : "-translate-y-[100%]"
       }`}
     >
       <div className="py-3 cursor-pointer w-fit ml-auto">
