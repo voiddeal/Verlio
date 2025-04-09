4e7f9a4d4d2de7489934efde4466d5575a92a375
Void Dealer <hr.shafiei.dev@gmail.com>
2025-04-09 23:15:05 +0330

    Let the UI below the coverAnimation be interactable

--- components/cover-layer-animation/CoverLayerAnimation.tsx
+++ components/cover-layer-animation/CoverLayerAnimation.tsx
@@ -5,18 +5,22 @@ import { useState } from "react"
 import "./cover-layer-animation.css"
 
 /* 
-apply the CoverLayer as a direct child of a container element.
+apply the CoverLayer as a direct child of its parent.
 the container element must be position: relative;
 */
 
 interface Props {
   to?: "left" | "right" | "top" | "bottom"
   follow?: boolean
+  threshold?: [number, number]
 }
 
-export default function CoverLayer({ to = "right", follow = false }: Props) {
+export default function CoverLayer({
+  to = "right",
+  follow = false,
+  threshold = [0, 0.6],
+}: Props) {
   const [isInView, setIsInView] = useState(false)
-  const threshold = [0, 0.5]
   const { ref } = useInView({
     threshold,
     triggerOnce: false,
@@ -30,7 +34,10 @@ export default function CoverLayer({ to = "right", follow = false }: Props) {
   })
 
   return (
-    <div ref={ref} className={`cover-layer-container`}>
+    <div
+      ref={ref}
+      className={`cover-layer-container ${isInView ? "hide" : ""}`}
+    >
       <div
         className={`cover-layer to-${to} ${follow ? "follow" : "no-follow"} ${
           isInView ? "active" : ""
