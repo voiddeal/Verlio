ef7629db4433121e3191b9ab48210a9a7191964b
Void Dealer <hr.shafiei.dev@gmail.com>
2025-04-09 23:34:51 +0330

    Update hero
    1. Use the new hook for parallax affect
    2. Add CTA cards

--- components/hero/Hero.tsx
+++ components/hero/Hero.tsx
@@ -1,41 +1,46 @@
 "use client"
 
-import useScrollTracker from "@/utils/useScrollTracker"
+import { useRef } from "react"
 import InfoCards from "../info-cards/InfoCards"
 import Image from "next/image"
+import CTA from "./CTA"
+import useScrollTracker from "@/utils/useScrollTracker"
+import useSmoothAnimation from "@/hooks/useViewportAnimation"
 
 export default function Hero() {
   const y = useScrollTracker()
+  const hero = useRef<HTMLDivElement>(null)
+  useSmoothAnimation({
+    targetValue: { y: y * 0.5 },
+    elementRef: hero,
+    factor: 1,
+  })
   return (
     <div className="relative z-30 w-dvw h-dvh overflow-hidden">
       {/* Hero Image */}
-      <div
-        className="absolute w-full h-full will-change-transform"
-        style={{
-          transform: `translateY(${y * 0.5}px)`,
-        }}
-      >
+      <div ref={hero} className="absolute w-full h-full will-change-transform">
         <Image
           id="hero-img"
           src="/hero/hero-image.jpg"
           fill
-          quality={100}
+          quality={80}
           alt="hero-image"
-          className="grayscale-[30%]  object-cover"
+          className="grayscale-[30%] object-cover"
           priority
         />
       </div>
       <div className="absolute inset-0 bg-gradient-to-b from-black/80 via-transparent to-transparent"></div>
-      <div className="absolute w-full h-full top-0 md:flex justify-between">
-        <h1 className="w-fit m-auto mt-[13dvh] text-white lg:scale-[1.5] 2xl:scale-[2] ">
+      <header className="absolute w-full h-full top-0 md:flex justify-between">
+        <h1 className="w-fit mb-auto mx-auto lg:mr-auto lg:ml-32 xl:ml-52 lg:mt-[20dvh] mt-[13dvh] text-white lg:scale-[1.5] 2xl:scale-[2] ">
           <span className="text-[10rem] leading-[11rem] monsieur-la-doulaise-regular">
             Verlio
           </span>
           <br />
           <span className="text-2xl leading-none capitalize">RESTAURANT</span>
         </h1>
+        <CTA />
         {/* <InfoCards /> */}
-      </div>
+      </header>
       {/* <Image
         src={"/verlio.png"}
         width={500}
