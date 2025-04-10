da01df900511138c5b117788e8cd8701ca65006f
Void Dealer <hr.shafiei.dev@gmail.com>
2025-04-11 00:14:37 +0330

    Add header Image for /reservation

--- app/reservation/page.tsx
+++ app/reservation/page.tsx
@@ -1,11 +1,19 @@
 import Heading from "@/components/Heading"
 import FirstStep from "@/components/reservation/first-step/FirstStep"
 import SecondStep from "@/components/reservation/second-step/SecondStep"
+import Image from "next/image"
 
 export default function ReservationsPage() {
   return (
     <main className="relative">
-      <header>
+      <header className="relative">
+        <Image
+          src={"/reservation.jpeg"}
+          fill
+          alt="image"
+          className="object-cover object-bottom"
+          priority
+        />
         <Heading heading="Reservation at Verlio" />
       </header>
       <section className="pb-16 px-4 bg-black bg-opacity-5">
