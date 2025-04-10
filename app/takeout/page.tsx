9aac38cbfa61a77c13e2a06539589e81efd98a97
Void Dealer <hr.shafiei.dev@gmail.com>
2025-04-10 22:45:59 +0330

    Update /takeout

--- app/takeout/page.tsx
+++ app/takeout/page.tsx
@@ -1,7 +1,7 @@
 export default function TakeoutPage() {
   return (
     <div className="flex justify-center items-center h-dvh">
-      <h1 className="text-4xl">UNDER CONSTRUCTION!</h1>
+      <h1 className="text-4xl">Not Implemented</h1>
     </div>
   )
 }
