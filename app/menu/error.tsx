970f7bd342b19343016fc431abdedb4035e5c586
Void Dealer <hr.shafiei.dev@gmail.com>
2025-04-10 22:43:23 +0330

    DRY please

--- app/menu/error.tsx
+++ app/menu/error.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useEffect } from "react"
+import ErrorHandler from "@/components/ErrorHandler"
 
 interface ErrorProps {
   error: Error
@@ -8,25 +8,5 @@ interface ErrorProps {
 }
 
 export default function Error({ error, reset }: ErrorProps) {
-  useEffect(() => {
-    console.error("Error:", error)
-  }, [error])
-
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 text-center px-4">
-      <h1 className="text-4xl font-bold text-red-500 mb-4">
-        Something went wrong
-      </h1>
-      <p className="text-gray-600 mb-6">
-        We’re sorry, but an unexpected error occurred. Please try again or
-        contact support if the issue persists.
-      </p>
-      <button
-        onClick={reset}
-        className="px-6 py-3 bg-blue-500 text-white rounded-md shadow-md hover:bg-blue-600 focus:outline-none"
-      >
-        Try Again
-      </button>
-    </div>
-  )
+  return <ErrorHandler error={error} reset={reset} />
 }
