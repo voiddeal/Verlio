ecdf9bd440dee6071c9b409f2117cbe4b6f9d698
Void Dealer <hr.shafiei.dev@gmail.com>
2025-04-11 21:34:24 +0330

    Add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next"
 import AppProviders from "@/providers/AppProviders"
+import { Analytics } from "@vercel/analytics/next"
 import "./globals.css"
 
 export const metadata: Metadata = {
@@ -27,6 +28,7 @@ export default function RootLayout({
           {children}
           {footer}
           {bottomNavbar}
+          <Analytics />
         </AppProviders>
       </body>
     </html>
